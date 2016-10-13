4a1b20348335b067984cc96afe4a23839d4640b2
Matt Wood <Matt@matts-mbp.fios-router.home>
2016-10-13 18:36:04 -0400

    Initial Commit

---
+++ Stock-App/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stock-App
+//
+//  Created by Matt Wood on 10/13/16.
+//  Copyright © 2016 MattWood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
